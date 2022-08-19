404417febfce6be0de27d031511d15248858e2e1
hikaru <alclimb@users.noreply.github.com>
2022-08-19 21:15:34 +0900

    feat: leafletを使った地図表示に対応

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script lang="ts" setup></script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    map
+    <ClientOnly>
+      <LazyAppLeaflet></LazyAppLeaflet>
+    </ClientOnly>
   </div>
 </template>
---
+++ components/AppLeaflet.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const mapEl = ref<HTMLElement>();
+
+onMounted(() => {
+  //
+  const map = L.map(mapEl.value).setView([37.9022418, 139.0235109], 13);
+
+  L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`, {
+    maxZoom: 19,
+    attribution: `© OpenStreetMap`,
+  }).addTo(map);
+
+  const marker = L.marker([37.9022418, 139.0235109], { opacity: 0.8 }).addTo(
+    map
+  );
+
+  marker.bindPopup(`<b>Hello world!</b><br>I am a popup.`);
+});
+</script>
+
+<template>
+  <div ref="mapEl" style="width: 1024px; height: 768px"></div>
+</template>
